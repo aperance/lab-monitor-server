e54fd26fa32d9f85dde61b33ff3c7d838248b282
William Aperance <aperance@gmail.com>
2018-02-22 21:05:15 -0800

    Restructured data sent on row update.

--- src/deviceStore.js
+++ src/deviceStore.js
@@ -24,46 +24,38 @@ class DeviceStore {
       throw new TypeError("Invalid Input");
     }
 
+    newState.timestamp = new Date()
+      .toLocaleString("en-US", this._dateFormat)
+      .replace(/,/g, "");
+
     const { state, history } = this._deviceData.get(id) || {
       state: {},
       history: {}
     };
 
     const modifiedKeys = Object.keys({ ...state, ...newState }) // Get array of all keys in both state objects
-      .filter(key => state[key] != newState[key]) // Remove properties that were not modified
-      .filter(key => key != "timestamp"); // Exclude timestamp from history
+      .filter(key => state[key] != newState[key]); // Remove properties that were not modified
 
     const modifiedState = modifiedKeys.reduce((acc, key) => {
       acc[key] = newState[key] || null;
       return acc;
     }, {});
 
-    const modifiedHistory = modifiedKeys.reduce((acc, key) => {
-      acc[key] = history[key] || []; // Copy property history to accumulator (or empty array if added property)
-      acc[key].unshift([Date.now(), newState[key] || null]); // Add new value to front of array (or null if property was removed)
-      while (acc[key].length > this._maxSize) acc[key].pop(); // Limit array length to maxSize setting
-      return acc;
-    }, {});
-    const options = {
-      weekday: "long",
-      year: "2-digit",
-      month: "numeric",
-      day: "numeric",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric"
-    };
-
-    const timestamp = new Date()
-      .toLocaleString("en-US", this._dateFormat)
-      .replace(/,/g, "");
+    const modifiedHistory = modifiedKeys
+      .filter(key => key != "timestamp") // Exclude timestamp from history
+      .reduce((acc, key) => {
+        acc[key] = history[key] || []; // Copy property history to accumulator (or empty array if added property)
+        acc[key].unshift([Date.now(), newState[key] || null]); // Add new value to front of array (or null if property was removed)
+        while (acc[key].length > this._maxSize) acc[key].pop(); // Limit array length to maxSize setting
+        return acc;
+      }, {});
 
     this._deviceData.set(id, {
-      state: { ...newState, timestamp },
+      state: newState,
       history: { ...history, ...modifiedHistory }
     });
 
-    this.emitter.emit("update", { [id]: modifiedState });
+    this.emitter.emit("update", { id, changes: modifiedState });
   }
 }
 
--- src/websocket.js
+++ src/websocket.js
@@ -1,12 +1,12 @@
 exports.createWebsocket = (io, deviceStore, config) => {
-  io.on("connection", function(socket) {
+  io.on("connection", socket => {
     console.log("websocket conneted");
     socket.emit("SET_CONFIGURATION", config.client);
     socket.emit("POPULATE_TABLE", deviceStore.getAllState());
   });
 
-  deviceStore.emitter.on("update", obj => {
+  deviceStore.emitter.on("update", updatedRow => {
     console.log("emitter fired");
-    io.emit("update", obj);
+    io.emit("UPDATE_ROW", updatedRow);
   });
 };
