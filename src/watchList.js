cca4124387dea2dd0e4a1f80be9a7c4bd08ee5f8
William Aperance <aperance@gmail.com>
2018-03-20 23:18:28 -0700

    Improved commenting.

--- src/watchList.js
+++ src/watchList.js
@@ -1,10 +1,28 @@
+// watchlist.js
+// William Aperance
+//
+// The WatchList class stores Map of IP addresses for desired devices, and ensures
+// that the devices are polled at the minimum specified interval. The startScanning
+// method is called, with poll function being passed, to begin operation.
+//
+
 class WatchList {
   constructor(config) {
+    // Create Map to store list of assets to monitor,
+    // with key = IP address and value = time of last poll.
     this._map = new Map();
+
+    // Function that starts polling cycle for a given IP address.
+    // Passed in by startScanning method.
     this._poll = null;
+
+    // When scanning, find devices that have not been polled since now minus _timeout.
     this._timeout = config.watchList.staleDataTimeout;
+
+    // How often to perform a scan.
     this._checkInterval = config.watchList.checkInterval;
 
+    // Cycle through provided ranges, adding IP address to Map.
     config.watchList.range.forEach(({ subnet, start, end }) => {
       for (let i = start; i <= end; i++) {
         this._map.set(subnet.slice(0, -1) + i, 0);
@@ -12,6 +30,15 @@ class WatchList {
     });
   }
 
+  // Called to initiate scan cycle. Externally defined poll function is passed in.
+  startScanning(poll) {
+    this._poll = poll;
+    setInterval(this._scan.bind(this), this._checkInterval);
+    this._scan();
+  }
+
+  // Find all devices in Map that have not been polled recently (now minus _timeout),
+  // and restart polling cycle for each. To be executed every checkInterval value.
   _scan() {
     return Array.from(this._map)
       .filter(([, timestamp]) => timestamp < Date.now() - this._timeout)
@@ -20,12 +47,8 @@ class WatchList {
       });
   }
 
-  startScanning(poll) {
-    this._poll = poll;
-    setInterval(this._scan.bind(this), this._checkInterval);
-    this._scan();
-  }
-
+  // Used by poll function to confirm that device with given IP address is still in Map,
+  // retuning true or false. If true, also updates timestamp in Map to current time.
   check(ipAddress) {
     if (this._map.has(ipAddress)) {
       this._map.set(ipAddress, Date.now());
@@ -33,10 +56,12 @@ class WatchList {
     } else return false;
   }
 
+  // Add new IP address to Map. Will begin polling at next scan.
   add(ipAddress) {
     if (!this._map.has(ipAddress)) this._map.set(ipAddress, 0);
   }
 
+  // Remove IP address from Map, preventing additional polling.
   delete(ipAddress) {
     this._map.delete(ipAddress);
   }
