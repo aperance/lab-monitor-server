7a8106fc68d7d6cbb2949a693b3de11ecf90cd25
William Aperance <aperance@gmail.com>
2018-04-25 22:20:46 -0700

    added check for unknown action request

--- src/actionHandler.js
+++ src/actionHandler.js
@@ -1,7 +1,9 @@
 exports.createActionHandler = (config, fetch) => {
   return (targets, type, { namespace, level }) => {
+    const action = config.actions[type] || null;
+    if(!action) return Promise.reject(new Error("Unknown action requested: " + type));
     const promiseArray = targets.map(ipAddress => {
-      let url = "http://" + ipAddress + config.actions[type].path;
+      let url = "http://" + ipAddress + action.path;
       if (type === "logLevel")
         url = url + "?namespace=" + namespace + "&level=" + level;
       console.log("Fetching " + url);
