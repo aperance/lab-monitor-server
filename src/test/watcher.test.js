4ed186e71691be0b122599cbec323cfff05a1099
William Aperance <aperance@gmail.com>
2018-06-23 20:12:19 -0700

    Added tests

---
+++ src/test/watcher.test.js
@@ -0,0 +1,37 @@
+jest.mock("got");
+
+jest.mock("../deviceStore.js", () => ({
+  default: {
+    set: jest.fn(),
+    setInactive: jest.fn()
+  }
+}));
+
+jest.mock(
+  "../../config.json",
+  () => ({
+    watcher: {
+      port: 9999,
+      path: "testPath",
+      sequenceKey: "testSequenceKey"
+    }
+  }),
+  { virtual: true }
+);
+
+const Watcher = require("../watcher.js").default;
+const deviceStore = require("../deviceStore.js").default;
+const got = require("got");
+const setTimeoutPromise = require("util").promisify(setTimeout);
+
+test("creates instance and begins operation", async () => {
+  got.mockImplementation(() => Promise.resolve({ body: "" }));
+  const watcher = new Watcher("127.0.0.1");
+  await setTimeoutPromise(100);
+  watcher.kill();
+  expect(got.mock.calls.length).toBe(1);
+  expect(got.mock.calls[0][0]).toEqual(
+    "http://127.0.0.1:9999/testPath?testSequenceKey=0"
+  );
+  expect(deviceStore.set.mock.calls.length).toBe(0);
+});
