690a235ef0cf8fbce7df64e254b32768ad56eb7f
William Aperance <aperance@gmail.com>
2018-07-06 17:56:39 -0700

    Dynamically create query string based on parameters. Check parameters against config.

--- src/actionHandler.ts
+++ src/actionHandler.ts
@@ -1,4 +1,5 @@
 import * as got from "got";
+import * as querystring from "querystring";
 import { actionHandler as log } from "./logger";
 
 const actionLookup: {
@@ -11,28 +12,37 @@ const actionLookup: {
 const actionHandler = (
   targets: string[],
   type: string,
-  parameters?: { [x: string]: string }
-) => {
-  const action = actionLookup[type];
-  if (action)
-    return Promise.all(
-      targets.map(ipAddress => {
-        let url = "http://" + ipAddress + action.path;
-        if (type === "logLevel" && parameters) {
-          url =
-            url +
-            "?namespace=" +
-            parameters.namespace +
-            "&level=" +
-            parameters.level;
-        }
-        log.info(url);
-        return got(url, { retries: 0 })
-          .then(res => true)
-          .catch(err => false);
-      })
-    );
-  else return Promise.reject(new Error("Unknown action requested: " + type));
+  parameters: { [x: string]: string } = {}
+): Promise<boolean[]> => {
+  const actionConfig = actionLookup[type];
+
+  if (!actionConfig)
+    return Promise.reject(new Error("Unknown action requested: " + type));
+
+  if (!checkParams(parameters, actionConfig.parameters))
+    return Promise.reject(new Error("Required parameters not provided"));
+
+  return Promise.all(
+    targets.map(ipAddress => {
+      let url = "http://" + ipAddress + actionConfig.path;
+      if (actionConfig.parameters.length !== 0)
+        url += "?" + querystring.stringify(parameters);
+      log.info(url);
+      return got(url, { retries: 0 })
+        .then(res => true)
+        .catch(err => false);
+    })
+  );
+};
+
+const checkParams = (paramObj: {}, paramConfig: string[]): boolean => {
+  const a = Object.keys(paramObj).sort();
+  const b = paramConfig.sort();
+  if (a.length !== b.length) return false;
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
 };
 
 export default actionHandler;
