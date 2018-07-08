cdf0d65b724d009e0eb29244e78a5a25ae18ffb9
William Aperance <aperance@gmail.com>
2018-07-07 17:35:49 -0700

    Restructured tests based on testCallback returned each cycle

--- src/test/watcher.test.js
+++ src/test/watcher.test.js
@@ -19,47 +19,63 @@ jest.mock(
   { virtual: true }
 );
 
+jest.useFakeTimers();
+
 const Watcher = require("../watcher.js").default;
 const deviceStore = require("../deviceStore.js").default;
 const got = require("got");
 const setTimeoutPromise = require("util").promisify(setTimeout);
 
+const body = {
+  testKey1: "value1",
+  testKey2: "value2",
+  testSequenceKey: "12345"
+};
+
 let watcher;
 
-test("creates instance and begins operation", async () => {
-  const body = {
-    testKey1: "value1",
-    testKey2: "value2",
-    testSequenceKey: "12345"
-  };
-  got.mockResolvedValueOnce({ body: `display(${JSON.stringify(body)})` });
-  got.mockResolvedValue({ body: `` });
-  //got.mockRejectedValue(new got.RequestError());
+test("creates instance and begins operation", done => {
   watcher = new Watcher("127.0.0.1");
+  got.mockResolvedValue({ body: `display(${JSON.stringify(body)})` });
+  watcher.testCallback = () => {
+    expect(got.mock.calls.length).toBe(1);
+    expect(got.mock.calls[0][0]).toEqual(
+      "http://127.0.0.1:9999/testPath?testSequenceKey=0"
+    );
+    expect(setImmediate).toHaveBeenCalledTimes(1);
+    expect(deviceStore.set.mock.calls.length).toBe(1);
+    expect(deviceStore.set.mock.calls[0]).toEqual([
+      "127.0.0.1",
+      "CONNECTED",
+      body
+    ]);
+    done();
+  };
   watcher.start();
-  await setTimeoutPromise(100);
-  expect(got.mock.calls.length).toBe(3);
-  expect(got.mock.calls[0][0]).toEqual(
-    "http://127.0.0.1:9999/testPath?testSequenceKey=0"
-  );
-  expect(got.mock.calls[1][0]).toEqual(
-    "http://127.0.0.1:9999/testPath?testSequenceKey=12345"
-  );
-  expect(got.mock.calls[2][0]).toEqual(
-    "http://127.0.0.1:9999/testPath?testSequenceKey=0"
-  );
-  expect(deviceStore.set.mock.calls.length).toBe(3);
-  expect(deviceStore.set.mock.calls[0]).toEqual([
-    "127.0.0.1",
-    "CONNECTED",
-    body
-  ]);
-  expect(deviceStore.set.mock.calls[1]).toEqual(["127.0.0.1", "RETRY"]);
-  expect(deviceStore.set.mock.calls[2]).toEqual(["127.0.0.1", "DISCONNECTED"]);
 });
 
-test("kill instance", () => {
+test("creates instance and begins operation", done => {
+  jest.clearAllMocks();
+  got.mockResolvedValue({ body: `display(${JSON.stringify(body)})` });
+  watcher.testCallback = () => {
+    expect(got.mock.calls.length).toBe(1);
+    expect(got.mock.calls[0][0]).toEqual(
+      "http://127.0.0.1:9999/testPath?testSequenceKey=12345"
+    );
+    expect(setImmediate).toHaveBeenCalledTimes(1);
+    expect(deviceStore.set.mock.calls.length).toBe(1);
+    expect(deviceStore.set.mock.calls[0]).toEqual([
+      "127.0.0.1",
+      "CONNECTED",
+      body
+    ]);
+    done();
+  };
+  jest.runOnlyPendingTimers();
+});
+
+test.skip("kill instance", () => {
   watcher.kill();
-  expect(deviceStore.set.mock.calls.length).toBe(4);
-  expect(deviceStore.set.mock.calls[3]).toEqual(["127.0.0.1", "INACTIVE"]);
+  expect(deviceStore.set.mock.calls.length).toBe(2);
+  expect(deviceStore.set.mock.calls[1]).toEqual(["127.0.0.1", "INACTIVE"]);
 });
