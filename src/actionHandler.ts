1fdb8cff319cdb295c0960f413a6a378c84c2597
William Aperance <aperance@gmail.com>
2018-07-11 20:08:09 -0700

    Refactored into seperate functions. Including errors into responses to client.

--- src/actionHandler.ts
+++ src/actionHandler.ts
@@ -1,3 +1,5 @@
+/** @module actionHandler */
+
 import * as got from "got";
 import * as querystring from "querystring";
 import { actionHandler as log } from "./logger";
@@ -9,40 +11,69 @@ const actionLookup: {
   };
 } = require("../config.json").actions;
 
-const actionHandler = (
-  targets: string[],
-  type: string,
-  parameters: { [x: string]: string } = {}
-): Promise<boolean[]> => {
-  const actionConfig = actionLookup[type];
-
-  if (!actionConfig)
-    return Promise.reject(new Error("Unknown action requested: " + type));
-
-  if (!checkParams(parameters, actionConfig.parameters))
-    return Promise.reject(new Error("Required parameters not provided"));
-
-  return Promise.all(
-    targets.map(ipAddress => {
-      let url = "http://" + ipAddress + actionConfig.path;
-      if (actionConfig.parameters.length !== 0)
-        url += "?" + querystring.stringify(parameters);
-      log.info(url);
-      return got(url, { retries: 0 })
-        .then(res => true)
-        .catch(err => false);
-    })
-  );
+/**
+ * Handles request from client to perform actions on devices.
+ * @async
+ * @param {actionRequest} action Type, parameters, and targets for action.
+ * @returns {Promise} Results to be sent back to client. Should not reject.
+ */
+const actionHandler = async (actionRequest: { [key: string]: any }) => {
+  log.info(actionRequest);
+  try {
+    validateParamaters(actionRequest);
+    const results = await sendRequests(actionRequest);
+    log.info(results);
+    return { err: null, results };
+  } catch (err) {
+    log.error(err);
+    return { err, results: null };
+  }
 };
 
-const checkParams = (paramObj: {}, paramConfig: string[]): boolean => {
-  const a = Object.keys(paramObj).sort();
-  const b = paramConfig.sort();
-  if (a.length !== b.length) return false;
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) return false;
-  }
-  return true;
+/**
+ * Ensure parameter names exactly match thoes listed in config file.
+ * @param {actionRequest}
+ * @throws {Error} on mismatch
+ */
+const validateParamaters = (actionRequest: { [x: string]: any }) => {
+  const actionConfig = actionLookup[actionRequest.type];
+  if (!actionConfig)
+    throw new Error("Unknown action type specified: " + actionRequest.type);
+  const expected = actionLookup[actionRequest.type].parameters.sort();
+  const received = Object.keys(actionRequest.parameters || {}).sort();
+  if (
+    expected.length !== received.length ||
+    !expected.every((param: string, index: number) => param === received[index])
+  )
+    throw new Error(`Invalid parameters for action: ${actionRequest.type}.
+      Expected ${expected}. Recevied ${received}.`);
+};
+
+/**
+ * Sends specified action request to all target devices.
+ * @param {actionRequest}
+ * @returns {Promise} Array of results for each request.
+ */
+const sendRequests = (actionRequest: { [x: string]: any }) => {
+  const { path, parameters } = actionLookup[actionRequest.type];
+  return Promise.all(
+    actionRequest.targets.map((ipAddress: string) => {
+      let url = "http://" + ipAddress + path;
+      if (parameters.length !== 0)
+        url += "?" + querystring.stringify(actionRequest.parameters);
+      log.info(url);
+
+      return (
+        got(url, { retries: 0 })
+          .then(res => ({ success: true, err: null }))
+          /**
+           * Promise not rejected on errors, so that
+           * parallel requests are not interrupted.
+           */
+          .catch(err => ({ success: false, err }))
+      );
+    })
+  );
 };
 
 export default actionHandler;
