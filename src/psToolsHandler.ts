8782bdb400f1c39f2f179177afc17bcd216ab5b2
William Aperance <aperance@gmail.com>
2018-12-30 21:46:28 -0800

    Moved from exec to execFile (part of transition to spawn)

--- src/psToolsHandler.ts
+++ src/psToolsHandler.ts
@@ -1,6 +1,6 @@
 /** @module psToolsHandler */
 
-import { exec } from "child_process";
+import { exec, execFile } from "child_process";
 import { getPsToolsConfig } from "./configuration";
 import { psToolsHandler as log } from "./logger";
 import { PsToolsRequest, PsToolsResponse } from "./types";
@@ -20,19 +20,19 @@ const psToolsHandler = async (
 ): Promise<PsToolsResponse> => {
   try {
     const { target, mode, argument } = request;
-    let command: string = "C:\\PSTools\\";
 
     if (typeof target !== "string")
       throw Error("Missing or invalid 'target' parameter");
     if (typeof argument !== "string")
       throw Error("Missing or invalid 'argument' parameter");
 
-    if (mode === "psExec") command += "psexec -d -i ";
-    else if (mode === "psKill") command += "pskill -t ";
+    const args = [`\\\\${target}`, `-u \\${user}`, `-p ${password}`, argument];
+    if (mode === "psExec") args.unshift("-d -i");
+    else if (mode === "psKill") args.unshift("-t");
     else throw Error("Missing or invalid 'mode' parameter");
+    const output = await executeCommand(mode, args);
 
-    command += `\\\\${target} -u \\${user} -p ${password} ${argument}`;
-    const output = await executeCommand(command);
+    const command = `\\\\${target} -u \\${user} -p ${password} ${argument}`;
     return { err: null, result: "$ " + command + "\r\n" + output };
   } catch (err) {
     log.error(err);
@@ -47,18 +47,27 @@ const psToolsHandler = async (
  * @param {string} command
  * @returns {Promise<string | Error>}
  */
-const executeCommand = (command: string): Promise<string | Error> => {
+const executeCommand = (
+  command: string,
+  args: string[]
+): Promise<string | Error> => {
   log.info("STDIN: " + command);
   return new Promise((resolve, reject) => {
-    exec(command, (err: any, stdout: string, stderr: string) => {
-      /** If error includes exit code, then command was successfull */
-      if (err && typeof err.code !== "number") reject(err);
-      else {
-        log.info("STDOUT: " + stdout);
-        log.info("STDERR: " + stderr);
-        resolve(stdout + stderr);
+    execFile(
+      command,
+      args,
+      // @ts-ignore
+      { cwd: "C:\\PSTools\\", shell: true },
+      (err: any, stdout: string, stderr: string) => {
+        /** If error includes exit code, then command was successfull */
+        if (err && typeof err.code !== "number") reject(err);
+        else {
+          log.info("STDOUT: " + stdout);
+          log.info("STDERR: " + stderr);
+          resolve(stdout + stderr);
+        }
       }
-    });
+    );
   });
 };
 
