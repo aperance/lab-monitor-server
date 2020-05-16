db3381b3dd081a6b3ed94b185bd3cd077867c450
William Aperance <aperance@gmail.com>
2020-05-16 00:40:45 -0700

    Heroku tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as express from "express";
+import * as http from "http";
+
 import engine from "./engine";
 import {startDemo} from "./demoEngine";
 import "./httpProxy";
@@ -5,34 +8,33 @@ import "./vncProxy";
 import "./websocket";
 
 if (process.env.DEMO === "true") startDemo();
-else engine.start();
+else {
+  engine.start();
 
-/*** Express ***/
+  /*** Express ***/
 
-import * as express from "express";
-import * as http from "http";
+  const app = express();
 
-// const app = express();
+  app.get("/refresh", (req, res) => {
+    engine.refresh();
+    res.send("OK");
+  });
 
-// app.get("/refresh", (req, res) => {
-//   engine.refresh();
-//   res.send("OK");
-// });
+  app.get("/gc", (req, res) => {
+    if (global.gc) {
+      global.gc();
+      res.send("OK");
+    } else res.send("You must run program with 'node --expose-gc index.js'");
+  });
 
-// app.get("/gc", (req, res) => {
-//   if (global.gc) {
-//     global.gc();
-//     res.send("OK");
-//   } else res.send("You must run program with 'node --expose-gc index.js'");
-// });
+  app.use(
+    express.static("public", {
+      setHeaders: (res, path, stat) => {
+        res.set("Access-Control-Allow-Origin", "*");
+      }
+    })
+  );
 
-// app.use(
-//   express.static("public", {
-//     setHeaders: (res, path, stat) => {
-//       res.set("Access-Control-Allow-Origin", "*");
-//     }
-//   })
-// );
-
-// const server = http.createServer(app);
-// server.listen(80);
+  const server = http.createServer(app);
+  server.listen(80);
+}
